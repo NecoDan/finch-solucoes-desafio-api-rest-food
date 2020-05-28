1cc45589ba08da3164ab016c119c3676007a400e
Daniel Santos <neco.daniel@gmail.com>
2020-05-28 09:44:31 -0300

    atualizacoes implementacao spring security

--- src/main/java/br/com/finch/api/food/config/SegurancaConfig.java
+++ src/main/java/br/com/finch/api/food/config/SegurancaConfig.java
@@ -3,6 +3,7 @@ package br.com.finch.api.food.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -22,8 +23,25 @@ public class SegurancaConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("api_finch_food").password("finch2020").roles("ADMIN");
+    protected void configure(HttpSecurity http) throws Exception {
+        // Para qualquer requisição (anyRequest) é preciso estar autenticado (authenticated).
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder authBuilder) throws Exception {
+        authBuilder.inMemoryAuthentication()
+                .withUser("api_finch_food")
+                .password("finch2020")
+                .roles("ADMIN")
+                .and()
+                .withUser("necodaniel")
+                .password("123")
+                .roles("USER");
     }
 
     @Bean
