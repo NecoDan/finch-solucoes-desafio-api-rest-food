527ba2c6bd6829cd877182b47bf352f9610ad72b
Daniel Santos <daniel.goncalves@armateus.com.br>
2020-07-01 13:34:57 -0300

    update classes de inicializacao spring

--- src/main/java/br/com/finch/api/food/App.java
+++ src/main/java/br/com/finch/api/food/App.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+/*@EnableTransactionManagement
+@EntityScan("br.com.curso.web.spring.devdojo.model")*/
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
